c3346880558352e3321c6b3a05e471c1718342fd
tomaszbrauntsch <tomaszbrauntsch@gmail.com>
2024-11-29 02:44:20 -0500

    added-as-6-fixed

--- assignment-6/src/App.js
+++ assignment-6/src/App.js
@@ -1,35 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
-// import stockData from './stockData.csv';
 import FileUpload from "./FileUpload";
-// import Child1 from "./Child1";
 import StackComp from "./Stackcomp.js";
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data : [{}]
+      data: [], // Initialized as an empty array
     };
   }
 
   set_data = (csv_data) => {
     this.setState({ data: csv_data });
-  }
+  };
 
   render() {
-    var mychild;
-    if (this.state.data.length === 0) {
-      mychild = null;
-    }
-    else {
-      mychild = <StackComp csv_data={this.state.data}></StackComp>
-    }
+    const { data } = this.state;
+
     return (
       <div>
-        <FileUpload set_data={this.set_data}></FileUpload>
+        <FileUpload set_data={this.set_data} />
         <div className="parent">
-          {mychild}
+          {/* Render StackComp only if data is not empty */}
+          {data.length > 0 && <StackComp csv_data={data} />}
         </div>
       </div>
     );
